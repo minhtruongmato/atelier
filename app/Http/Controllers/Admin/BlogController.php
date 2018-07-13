124a778c76aca74c5ee6c1e62bdf11d689783b5a
minhtruongmato <truong.do@matocreative.vn>
2018-07-13 11:23:34 +0700

    [done] remove junk files

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -219,18 +219,6 @@ class BlogController extends Controller
     private function validateInput($request) {
         $this->validate($request, [
             'title' => 'required|max:60',
-//            'price' => 'required|max:60',
-//            'middlename' => 'required|max:60',
-//            'address' => 'required|max:120',
-//            'city_id' => 'required',
-//            'state_id' => 'required',
-//            'country_id' => 'required',
-//            'zip' => 'required|max:10',
-//            'age' => 'required',
-//            'birthdate' => 'required',
-//            'date_hired' => 'required',
-//            'department_id' => 'required',
-//            'division_id' => 'required'
         ]);
     }
 }
