0fa135b29754d426b97ad60f3327145a7cb3bb44
hungluong249 <hung.luong@matocreative.vn>
2018-07-24 10:39:24 +0700

    implement list n detail product view

--- resources/views/list-trends.blade.php
+++ resources/views/list-trends.blade.php
@@ -45,21 +45,22 @@
 
         <section id="list-trends">
             <div class="container">
-            <div masonry load-images="true">
+                <div masonry load-images="true">
 
-                <div class="masonry-brick <% $index%2==0 ? 'masonry-brick-2' : '' %>" ng-repeat="trend in trends">
-                    <a href="" target="_self">
-                        <div class="inner">
-                            <div class="mask">
-                                <img ng-src="{{ asset('storage/app/trends') }}<% '/' + trend.slug + '/' + trend.image %>" alt="A masonry brick">
+                    <div class="masonry-brick <% $index%2==0 ? 'masonry-brick-height-2' : '' %>" ng-repeat="trend in trends">
+                        <a href="" target="_self">
+                            <div class="inner">
+                                <div class="mask">
+                                    <img ng-src="{{ asset('storage/app/trends') }}<% '/' + trend.slug + '/' + trend.image %>" alt="A masonry brick">
 
-                                <div class="content">
-                                    <h4 class="post-subheading dark"><% trend.cate_title %></h4>
-                                    <h2 class="post-heading dark"><% trend.title %></h2>
+                                    <div class="content">
+                                        <h4 class="post-subheading dark"><% trend.cate_title %></h4>
+                                        <h2 class="post-heading dark"><% trend.title %></h2>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </a>
+                        </a>
+                    </div>
                 </div>
             </div>
 
